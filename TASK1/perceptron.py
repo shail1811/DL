5964c6945630458129f85e08653a0b237411eb89
shail1811 <46260688+shail1811@users.noreply.github.com>
2020-07-31 18:32:57 +0530

    Add files via upload

---
+++ TASK1/perceptron.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+class Perceptron(object):
+    def __init__(self,no_of_inputs,threshold = 6,learning_rate=1):
+        self.threshold = threshold
+        self.learning_rate = learning_rate
+        self.weights = np.zeros(no_of_inputs + 1)
+        
+    def predict(self,inputs):
+        summation = np.dot(inputs, self.weights[1:]) + self.weights[0]
+        if summation > 0 :
+            activation = 1
+        else:
+            activation = 0
+        return activation
+    
+    def train(self , training_inputs , labels):
+        for _ in range(self.threshold):
+            print("Iteration No." + str(_))
+            print(self.weights)
+            for inputs,label in zip(training_inputs,labels):
+                prediction = self.predict(inputs)
+                self.weights[1:] += self.learning_rate * (label - prediction) * inputs
+                self.weights[0] += self.learning_rate * (label - prediction)
+            
